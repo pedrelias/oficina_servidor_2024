45fb05db251103cae006f11516127e6ea102600c
pedrelias <pedreliasz224@gmail.com>
2024-07-24 15:47:27 -0300

    mudança titulo pagina

--- teste_server/index.php
+++ teste_server/index.php
@@ -3,10 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Usuários</title>
+    <title>Oficina de Servidores Web</title>
 </head>
 <body>
-    <h1>Cadastro de Usuários</h1>
+    <h1>Oficina de Servidores Web</h1>
+    <h2>Cadastro de Usuários</h2>
     <form action="registro.php" method="post">
         Nome: <input type="text" name="nome" required><br>
         Email: <input type="email" name="email" required><br>
